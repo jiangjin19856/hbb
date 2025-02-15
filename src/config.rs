e3956ecd407101a7517bf9117d1c7e58c3547ff7
jiangjin19856 <161402309+jiangjin19856@users.noreply.github.com>
2025-02-15 14:25:59 +0800

    更新服务器信息

--- src/config.rs
+++ src/config.rs
@@ -102,7 +102,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["jiangjinyun.top"];
-pub const RS_PUB_KEY: &str = "123456";
+pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
