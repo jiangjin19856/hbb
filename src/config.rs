646664c8bf4a3730b976b6881feb4192523d326e
jiangjin19856 <147308164@qq.com>
2025-02-28 11:55:46 +0800

    up api

--- src/config.rs
+++ src/config.rs
@@ -104,6 +104,7 @@ const CHARS: &[char] = &[
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["jiangjinyun.top"];
 pub const RS_PUB_KEY: &str = "jiangjinyun";
+pub const API_SERVER: &[&str] = &["https://jiangjinyun.top:21114"];
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
